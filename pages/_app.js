d63e8a1cf42e96f5f321e26bdef852a4e482426d
amlan950 <amlanbiswal9@gmail.com>
2025-06-20 20:33:43 +0530

    Add firebase config5

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { JobProvider } from "../context/Contexxt";
-import "@/styles/globals.css";
-import Navbar from "@/Components/NavBar";
+
+
 
 function App({ Component, pageProps }) {
   return (
